44274b7cf39b0e6f892abb129dd79b189a58a998
Roman Yerchyk <lann.wauthority@gmail.com>
2025-08-02 19:09:14 +0300

    feat: use web history

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { createMemoryHistory, createRouter } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import MainScreen from './screens/MainScreen.vue';
 import LoginScreen from './screens/LoginScreen.vue';
 import RegisterScreen from './screens/RegisterScreen.vue';
 
 createApp(App)
   .use(createRouter({
-    history: createMemoryHistory(),
+    history: createWebHistory(),
     routes: [
       { path: '/', component: MainScreen },
       { path: '/login', component: LoginScreen },
